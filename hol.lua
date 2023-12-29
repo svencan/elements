84a8d51bb46e4d5ff5f3263f9dc34d9713e0c05b
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-29 16:19:20 +0100

    Pick hydrogen

--- hol.lua
+++ hol.lua
@@ -7,4 +7,26 @@ args = {...}
 hydro = args[1]
 oxy = args[2]
 
-print("Requesteddddd "..args[1].." hdydrogen and "..args[2].." oxygen")
+print("Requested "..args[1].." hydrogen and "..args[2].." oxygen")
+
+-- Hydrogen at X=1314
+-- Oxygen at X=1313
+-- Dropoff at X=1321
+
+while hydro do
+	turtle.turnLeft()
+	turtle.turnLeft()
+	turtle.forward()
+	turtle.turnLeft()
+	turtle.suck(1)
+
+	picked = turtle.getItemCount()
+	print("I have "..picked.." hydrogen")
+	hydro = hydro - picked
+	print("Still need "..hydro.." hydrogen")
+
+	turtle.turnLeft()
+	turtle.forward()
+	turtle.drop(1)
+
+print("I collected all the hydrogen.")
