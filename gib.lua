02a2123d3ae23e3ecb491b5b1d483062e8ecb5a6
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-29 23:01:21 +0100

    Breakdown of the elements required

--- gib.lua
+++ gib.lua
@@ -5,11 +5,11 @@ dofile("/elements.lua")
 args = {...}
 
 if type(next(args)) == "nil" then
-	io.write("Type the element-number you want")
+	print("Type the element-number you want")
 	io.write("e.g. 79 for Gold: ")
     element = tonumber(io.read())
 
-	io.write("You want " .. elements[element] .. "")
+	print("You want " .. elements[element] .. "")
 	io.write("How much? (e.g. 16 for one ingot): ")
     amount = tonumber(io.read())
 else
@@ -20,6 +20,29 @@ end
 element_name = elements[element]
 element_id = "chemlib:" .. string.lower(elements[element])
 
+ga = element / 64
+remainder = element - ga * 64
 
+ge = remainder / 32
+remainder = remainder - ge * 32
 
+s = remainder / 16
+remainder = remainder - s * 16
 
+o = remainder / 8
+remainder = remainder - o * 8
+
+be = remainder / 4
+remainder = remainder - be * 4
+
+he = remainder / 2
+h = h - he * 2
+
+print("You will need")
+print("  Gadolinium: " .. ga * amount)
+print("  Germanium:  " .. ge * amount)
+print("  Sulfur:     " .. s * amount)
+print("  Oxygen:     " .. o * amount)
+print("  Beryllium:  " .. be * amount)
+print("  Helium:     " .. he * amount)
+print("  Hydrogen:   " .. h * amount)
