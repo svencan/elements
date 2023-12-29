ed2463e79e2a10ec0f4a1991dace76b4ce91f428
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-29 19:31:53 +0100

    Check element table

--- mach.lua
+++ mach.lua
@@ -7,6 +7,8 @@ print("Turtle refuelled.")
 -- Input parameters
 args = {...}
 element = tonumber(args[1])
+element_name = elements[element]
+element_id = "chemlib:" .. string.lower(elements[element])
 amount = tonumber(args[2])
 remaining = amount
 
@@ -20,24 +22,20 @@ while remaining > 0 do
 	turtle.select(2)
 	turtle.suck()
 
-	if turtle.getItemCount() > 0 then
-		slot_data = turtle.getItemDetail()
-		print("Picked up " .. slot_data.name)
-		dump(slot_data)
-	end
-
 	-- go to final chest
 	turtle.turnLeft()
 	turtle.forward()
 
-	-- if slot 2 is element:
-	--   turn right
-	--   remaining = remaining - item count of slot 2
-	--   drop slot 2
-	--   turn left
+	if turtle.getItemCount() > 0 then
+		slot_data = turtle.getItemDetail()
+		if slot_data.name == element_id then
+			turtle.turnRught()
+			turtle.drop()
+			turtle.turnLeft()
+		end
+	end
 
 	-- Go to fusion input
-	
 	turtle.forward()
 	turtle.forward()
 	turtle.forward()
