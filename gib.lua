f3cdb6216cd0f710c1a3ba3267af6f3ef1c11996
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-29 16:59:08 +0100

    Requirement calculation

--- gib.lua
+++ gib.lua
@@ -1,1 +1,12 @@
-print("Hello world (gib.lua)")
+args = {...}
+
+element = tonumber(args[1])
+amount = tonumber(args[2])
+
+hydrogen_part = element % 8
+hydrogen_total = hydrogen_part * amount
+
+oxygen_part = math.floor(element / 8)
+oxygen_total = oxygen_part * amount
+
+print("We will need " .. oxygen_total .. " oxygen and " .. hydrogen_total .. " hydrogen.")
