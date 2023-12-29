46841e0335f353ad6a04c37e03c80b13da6d86cb
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-29 22:46:21 +0100

    gib promp

--- gib.lua
+++ gib.lua
@@ -4,8 +4,20 @@ rednet.open("top")
 
 args = {...}
 
-element = tonumber(args[1])
+if type(next(myTable)) == "nil" then
+	io.write("Type the element-number you want (e.g. 79 for gold)")
+    element = tonumber(io.read())
+
+	io.write("You want " .. elements[element] .. ". How much of it? (16 for 1 ingot)")
+    amount = tonumber(io.read())
+else
+	element = tonumber(args[1])
+	amount = tonumber(args[2])
+end
+
 element_name = elements[element]
 element_id = "chemlib:" .. string.lower(elements[element])
-amount = tonumber(args[2])
+
+
+
 
