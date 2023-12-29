a4e2488dd464ed13ade8292fc1f8b8b02771ab82
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-29 16:47:16 +0100

    Pick oxygen and hydrogen

--- hol.lua
+++ hol.lua
@@ -2,32 +2,45 @@
 turtle.refuel()
 print("Turtle refuelled.")
 
-
 args = {...}
-hydro = tonumber(args[1])
-oxy = tonumber(args[2])
+hydrogen = tonumber(args[1])
+oxygen = tonumber(args[2])
 
 print("Requested "..args[1].." hydrogen and "..args[2].." oxygen")
 
--- Hydrogen at X=1314
--- Oxygen at X=1313
--- Dropoff at X=1321
+while hydrogen > 0 or oxygen > 0 do
+	turtle.turnLeft()
+	turtle.turnLeft()
 
-while hydro > 0 do
-	turtle.turnLeft()
-	turtle.turnLeft()
+	-- Pick hydrogen
+	turtle.select(10)
 	turtle.forward()
 	turtle.turnLeft()
-	turtle.suck(hydro)
+	turtle.suck(hydrogen)
+	hydrogen = hydrogen - turtle.getItemCount()
+	print("Picked " .. turtle.getItemCount() .. " hydrogen")
+	turtle.turnRight()
 
-	picked = turtle.getItemCount()
-	print("I have "..picked.." hydrogen")
-	hydro = hydro - picked
-	print("Still need "..hydro.." hydrogen")
+	-- Pick oxygen
+	turtle.select(11)
+	turtle.forward()
+	turtle.turnLeft()
+	turtle.suck(oxygen)
+	oxygen = oxygen - turtle.getItemCount()
+	print("Picked " .. turtle.getItemCount() .. " oxygen")
 
+	-- Go to drop off
 	turtle.turnLeft()
 	turtle.forward()
-	turtle.drop(picked)
+	turtle.forward()
+	
+	-- Drop hydrogen
+	turtle.select(10)
+	turtle.drop(turtle.getItemCount())
+	
+	-- Drop oxygen
+	turtle.select(11)
+	turtle.drop(turtle.getItemCount())
 end
 
-print("I collected all the hydrogen.")
+print("*happy turtle noises*")
