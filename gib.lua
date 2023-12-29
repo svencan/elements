505d22ce392eb597007b24f9d0bf17f955f71870
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-29 22:49:11 +0100

    Better prompt

--- gib.lua
+++ gib.lua
@@ -1,14 +1,16 @@
 dofile("/elements.lua")
 
-rednet.open("top")
+--rednet.open("top")
 
 args = {...}
 
-if type(next(myTable)) == "nil" then
-	io.write("Type the element-number you want (e.g. 79 for gold)")
+if type(next(args)) == "nil" then
+	io.write("Type the element-number you want")
+	io.write("e.g. 79 for Gold: ")
     element = tonumber(io.read())
 
-	io.write("You want " .. elements[element] .. ". How much of it? (16 for 1 ingot)")
+	io.write("You want " .. elements[element] .. "")
+	io.write("How much? (e.g. 16 for one ingot): ")
     amount = tonumber(io.read())
 else
 	element = tonumber(args[1])
