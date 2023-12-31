932abd5ed2d6d829ec1fbf9fdeadb4ff637a81ca
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-31 13:12:25 +0100

    Fix mach

--- mach.lua
+++ mach.lua
@@ -15,16 +15,18 @@ end_product = periphals.wrap("top")    -- end product
 
 while tonumber(remaining) > 0 do
 	local output_slot = 1
-	for n=1,reactor_output.size()-1, 1
-	shell.run("sleep(1.03)")
-	details = reactor_output.getItemDetails(output_slot)
-	if details.name == element_id then
-		pulled = reactor_output.pushItems("top", output_slot)
-		remaining = remaining - pulled
-	else
-		reactor_output.pushItems("left", output_slot)
+	sleep(1.03)
+	for n=1,reactor_output.size()-1, 1 do
+	
+		details = reactor_output.getItemDetails(output_slot)
+		if details.name == element_id then
+			pulled = reactor_output.pushItems("top", output_slot)
+			remaining = remaining - pulled
+		else
+			reactor_output.pushItems("left", output_slot)
+		end
+		output_slot = output_slot + 1
 	end
-	output_slot = output_slot + 1
 end
 
 print("Finished everything")
