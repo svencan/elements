0355ee0eb8d005ac263977c782c67955783cea72
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-31 13:42:47 +0100

    Use reactor output list

--- mach.lua
+++ mach.lua
@@ -17,17 +17,28 @@ while tonumber(remaining) > 0 do
 	print("Checking all slots for reactor outputs")
 	local output_slot = 1
 	sleep(1.03)
-	for n=1,reactor_output.size()-1, 1 do
-		if reactor_output.getItemLimit(output_slot) < 64 then
-			details = reactor_output.getItemDetails(output_slot)
-			if details.name == element_id then
+	for slot, item in pairs(reactor_output.list()) do
+		print(("%d x %s in slot %d"):format(item.count, item.name, slot))
+		if item.count > 0 then
+			if item.name == element_id then
 				pulled = reactor_output.pushItems("top", output_slot)
 				remaining = remaining - pulled
 			else
 				reactor_output.pushItems("left", output_slot)
 			end
 		end
-		output_slot = output_slot + 1
+	end
+	--for n=1,reactor_output.size()-1, 1 do
+	--	if reactor_output.getItemLimit(output_slot) < 64 then
+	--		details = reactor_output.getItemDetails(output_slot)
+	--		if details.name == element_id then
+	--			pulled = reactor_output.pushItems("top", output_slot)
+	--			remaining = remaining - pulled
+	--		else
+	--			reactor_output.pushItems("left", output_slot)
+	--		end
+	--	end
+	--	output_slot = output_slot + 1
 	end
 end
 
