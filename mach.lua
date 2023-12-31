52c5aac7a557f96bd7e3511ec5c9c5ba4cd55a71
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-31 14:56:15 +0100

    Wait for input to be emptied first

--- mach.lua
+++ mach.lua
@@ -1,5 +1,13 @@
 dofile("/elements.lua")
 
+function inventory_sum(inventory)
+	local sum = 0
+	for slot, item in pars(inventory.list()) do
+		sum = sum + item.count
+	end
+	return sum
+end
+
 args = {...}
 
 element = args[1]
@@ -15,14 +23,18 @@ end_product = peripheral.wrap("top")    -- end product
 
 while tonumber(remaining) > 0 do
 	local output_slot = 1
-	sleep(1.03)
-	for slot, item in pairs(reactor_output.list()) do
-		if item.count > 0 then
-			if item.name == element_id then
-				pulled = reactor_output.pushItems("top", output_slot)
-				remaining = remaining - pulled
-			else
-				reactor_output.pushItems("left", output_slot)
+
+	sleep(1.01)
+
+	if inventory_sum(reactor_input) == 0 then
+		for slot, item in pairs(reactor_output.list()) do
+			if item.count > 0 then
+				if item.name == element_id then
+					pulled = reactor_output.pushItems("top", output_slot)
+					remaining = remaining - pulled
+				else
+					reactor_output.pushItems("left", output_slot)
+				end
 			end
 		end
 	end
