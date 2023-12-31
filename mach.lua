67456d24d07614078c310ed865bfe1f57413bf4c
Sven Cannivy <sven.cannivy@gmail.com>
2023-12-31 13:25:36 +0100

    Check if there are items

--- mach.lua
+++ mach.lua
@@ -17,13 +17,14 @@ while tonumber(remaining) > 0 do
 	local output_slot = 1
 	sleep(1.03)
 	for n=1,reactor_output.size()-1, 1 do
-	
-		details = reactor_output.getItemDetails(output_slot)
-		if details.name == element_id then
-			pulled = reactor_output.pushItems("top", output_slot)
-			remaining = remaining - pulled
-		else
-			reactor_output.pushItems("left", output_slot)
+		if reactor_output.getItemCount(output_slot) > 0 then
+			details = reactor_output.getItemDetails(output_slot)
+			if details.name == element_id then
+				pulled = reactor_output.pushItems("top", output_slot)
+				remaining = remaining - pulled
+			else
+				reactor_output.pushItems("left", output_slot)
+			end
 		end
 		output_slot = output_slot + 1
 	end
